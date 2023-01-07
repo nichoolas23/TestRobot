ce1420666311496bee78e69733bdd9e65bbff228
ntornberg <NTornberg@metalexchangecorp.com>
2023-01-07 17:44:24 -0600

    SLAM

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -22,6 +22,7 @@ public final class Main
     */
     public static void main(String... args)
     {
+       
         RobotBase.startRobot(Robot::new);
     }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,16 +5,20 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import frc.robot.CompVision.ComputerVision;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.*;
 
-import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -25,14 +29,16 @@ import java.awt.event.KeyEvent;
  */
 public class Robot extends TimedRobot
 {
-
+    final double ANGULAR_P = 0.1;
+    final double ANGULAR_D = 0.0;
+    PIDController turnController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
     private final WPI_TalonSRX[] wpi_talonSRXES = new WPI_TalonSRX[]{new WPI_TalonSRX(1),new WPI_TalonSRX(3),new WPI_TalonSRX(2),new WPI_TalonSRX(4)};
-    
     private final MotorControllerGroup leftDrive = new MotorControllerGroup(wpi_talonSRXES[0],wpi_talonSRXES[1]);
     private final MotorControllerGroup rightDrive = new MotorControllerGroup(wpi_talonSRXES[2],wpi_talonSRXES[3]);
-    //private final MotorControllerGroup leftDrive = new DifferentialDrive(leftDrive, rightDrive);
+    private static double rotateVal;
     private final XboxController xboxController = new XboxController(0);
     private final Timer timer = new Timer();
+    private static final PhotonCamera camera = new PhotonCamera("photonvision");
     
     
     /**
@@ -46,6 +52,10 @@ public class Robot extends TimedRobot
         // result in both sides moving forward. Depending on how your robot's
         // gearbox is constructed, you might have to invert the left side instead.
         rightDrive.setInverted(true);
+        ComputerVision initCompVision = new ComputerVision();
+        Runnable init = initCompVision::init;
+        init.run();
+    
     }
     
     
@@ -73,48 +83,60 @@ public class Robot extends TimedRobot
     public void teleopInit() {
         leftDrive.set(0);
         rightDrive.set(0);
-     
-        
-      
     }
     @Override
     public void disabledPeriodic(){}
     
     @Override
     public void robotPeriodic(){
-    
+        var result = camera.getLatestResult();
+        if(result.hasTargets()){
+            List<PhotonTrackedTarget> targets = result.getTargets();
+            PhotonTrackedTarget bestTarget = result.getBestTarget();
+            rotateVal = -turnController.calculate(bestTarget.getYaw(), 0);
+//TODO: setup co-processor
+        }
     }
     /** This method is called periodically during teleoperated mode. */
     @Override
     public void teleopPeriodic()
     {
-        DriverStation.reportWarning("teleperiodic",false);
         if(xboxController.getAButtonPressed()){
             leftDrive.stopMotor();
             rightDrive.stopMotor();
         }
-        double forward = xboxController.getLeftTriggerAxis()*.4;
-        double reverse = xboxController.getRightTriggerAxis()*.4;
-        double rotate = xboxController.getRightX()*.4;
-        
-    
-        leftDrive.set(forward+rotate);
-        rightDrive.set(forward-rotate);
-        
-        leftDrive.set(reverse+rotate);
-        rightDrive.set(reverse-rotate);
-      
         
         
+        double forward = xboxController.getLeftTriggerAxis()*.2;
+        double reverse = xboxController.getRightTriggerAxis()*.2;
+        rotateVal = xboxController.getRightX()*.2;
+        
+        leftDrive.set(forward+rotateVal);
+        rightDrive.set(forward-rotateVal);
+        
+        leftDrive.set(reverse+rotateVal);
+        rightDrive.set(reverse-rotateVal);
     }
     
     
-    /** This method is called once each time the robot enters test mode. */
+  
+    
+    //TODO: Setup photonvision sim testing
     @Override
-    public void testInit() {}
+    public void testInit() {
     
+    }
+  
+    @Override
+    public void simulationInit() {
     
-    /** This method is called periodically during test mode. */
+        
+    }
+    
+    @Override
+    public void simulationPeriodic() {
+    }
+    
     @Override
     public void testPeriodic() {}
 }
